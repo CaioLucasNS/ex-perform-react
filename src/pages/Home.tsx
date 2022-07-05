da164477b1ba4bf02a8a095b6a18ded89cbfd688
DESKTOP-PKAI836\Caio <caio.lucas.do.silva@accenture.com>
2022-07-05 12:57:07 -0300

    added list and search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,39 @@
+import { FormEvent, useState } from "react";
+import { SearchResults } from "../components/SearchResults";
+
 export function Home() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      // case search is empty and disregarding empy characters
+      // return - stop the execution
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
   return (
     <div>
-      <h2>Home</h2>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
     </div>
   );
 }
