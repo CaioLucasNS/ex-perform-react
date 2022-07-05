e8ab6a230ec00d0a17efb5806452a7b5612abf1d
DESKTOP-PKAI836\Caio <caio.lucas.do.silva@accenture.com>
2022-07-05 11:48:41 -0300

    initial commit

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,40 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "../pages/Home";
+
+export function AppRoutes() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Routes> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
