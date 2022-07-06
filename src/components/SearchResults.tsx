92696d888b3bcab0490e0cef054ef7b37e64f9c9
DESKTOP-PKAI836\Caio <caio.lucas.do.silva@accenture.com>
2022-07-06 11:27:13 -0300

    added useCallback

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -7,9 +7,13 @@ interface SearchResultsProps {
     price: number;
     title: string;
   }>;
+  onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults({ results }: SearchResultsProps) {
+export function SearchResults({
+  results,
+  onAddToWishList,
+}: SearchResultsProps) {
   const totalPrice = useMemo(() => {
     return results.reduce((total, product) => {
       return total + product.price;
@@ -21,14 +25,12 @@ export function SearchResults({ results }: SearchResultsProps) {
       <h2>{totalPrice}</h2>
 
       {results.map((product) => (
-        <ProductItem key={product.id} product={product} />
+        <ProductItem
+          key={product.id}
+          product={product}
+          onAddToWishList={onAddToWishList}
+        />
       ))}
     </div>
   );
 }
-
-/**
- * Quando usar o useMemo?
- * 1. Cálculos pesados;
- * 2. Igualdade referencial (quando a gente repassa aquela informação a um componente filho);
- */
